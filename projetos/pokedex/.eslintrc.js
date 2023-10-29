282436f701f39c32e2738d3ed54b4bbd1b6131ed
rafaelbporto <rafaelbporto88@gmail.com>
2023-10-29 16:10:46 -0300

    eslint

--- projetos/pokedex/.eslintrc.js
+++ projetos/pokedex/.eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'], // Defina como 'PascalCase' para desativar o aviso
   },
 };
