07d77a5d03181d6ccc20b2c0c3f8953286a0e6ce
rafaelbporto <rafaelbporto88@gmail.com>
2023-10-29 16:12:12 -0300

    compoent app

--- projetos/pokedex/src/App.vue
+++ projetos/pokedex/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="pokedex">
-    <h1 class="text-yellow">
-      Pokedex
-    </h1>
+    <Pokedex />
   </div>
 </template>
 
 <script>
+import Pokedex from './components/Pokedex.vue';
 
 export default {
   name: 'App',
+  components: {
+    Pokedex,
+  },
 };
 </script>
 
@@ -17,9 +19,9 @@ export default {
 #pokedex {
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 24px;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 20px);
+  padding: 20px;
 }
 </style>
